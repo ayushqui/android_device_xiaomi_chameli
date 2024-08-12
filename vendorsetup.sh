a686795e291c273b6f68e664ed910a3532128d2e
lordgaruda <falgunpatel.fv@gmail.com>
2024-08-12 12:13:01 +0000

    camellia: init vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+# Kernel 
+git clone --depth=1 https://github.com/MI-CHAMELI/device_xiaomi_chameli-kernel device/xiaomi/camellia-kernel
+
+# Device Vendor Tree
+git clone --depth=1 https://github.com/dm700-devs/vendor_xiaomi_camellia.git vendor/xiaomi/camellia -b fourteen
+
+# Sepolicies
+git clone --depth=1 https://github.com/dm700-devs/device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr 
+
+# Hardware Mediatek
+git clone --depth=1 https://github.com/lordgaruda/hardware_mediatek.git hardware/mediatek
+
+# Hardware Xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+
