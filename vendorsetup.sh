774ad228ec85d73a8cf7517ba620febddb2162b6
ayushqui <113664933+ayushqui@users.noreply.github.com>
2024-09-15 14:11:27 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Kernel 
-git clone --depth=1 https://github.com/MI-CHAMELI/device_xiaomi_chameli-kernel device/xiaomi/camellia-kernel
+git clone https://github.com/dm700-devs/device_xiaomi_camellia-kernel.git -b thirteen device/xiaomi/camellia-kernel
 
 # Device Vendor Tree
 git clone --depth=1 https://github.com/dm700-devs/vendor_xiaomi_camellia.git vendor/xiaomi/camellia -b fourteen
@@ -8,8 +8,10 @@ git clone --depth=1 https://github.com/dm700-devs/vendor_xiaomi_camellia.git ven
 git clone --depth=1 https://github.com/dm700-devs/device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr 
 
 # Hardware Mediatek
-git clone --depth=1 https://github.com/lordgaruda/hardware_mediatek.git hardware/mediatek
+git clone https://github.com/PQEnablers-Devices/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
 
 # Hardware Xiaomi
 git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 
+#viperfx
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
